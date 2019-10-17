4f5e578b932f9c21219d0f1b224810c993b2bbfe
Synxsaints <Synxsaints@LAPTOP-EAMMNJ87>
2019-10-17 22:08:36 +0700

    First Commit

---
+++ src/pbo2/pkg10118060/latihan/ke/pkg39/nilaimaxmin/Petugas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118060.latihan.ke.pkg39.nilaimaxmin;
+
+/**
+ *
+ * @author Permai 
+ */
+class Petugas {
+    private String Nama;
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String Nama) {
+        this.Nama = Nama;
+    }
+    
+}
